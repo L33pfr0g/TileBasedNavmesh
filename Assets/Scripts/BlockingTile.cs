56fba0f8b811371195db4df46a48e196c90dad30
nmorasse <nathaniel.morasseg@gmail.com>
2018-09-04 23:03:06 -0400

    initial commit

---
+++ Assets/Scripts/BlockingTile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System;
+
+[Serializable]
+[CreateAssetMenu(fileName = "New Blocking Tile", menuName = "Tiles/Blocking Tile")]
+public class BlockingTile : TileBase
+{
+}
---
+++ Assets/Scripts/CellToNavmesh.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CellToNavmesh : MonoBehaviour
+{
+
+    private Grid grid;
+    private Tilemap map;
+    // Use this for initialization
+    void Start()
+    {
+        grid = GetComponent<Grid>();
+        map = GetComponentInChildren<Tilemap>();
+
+        InitColliders();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void InitColliders()
+    {
+        for (int x = map.origin.x; x < map.size.x; x++)
+        {
+            for (int y = map.origin.y; y < map.size.y; y++)
+            {
+                Debug.Log(String.Format("({0},{1},0) : {2}", x, y, map.GetTile(new Vector3Int(x, y, 0))));
+
+            }
+        }
+    }
+}
